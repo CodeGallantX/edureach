374577daa1a317070c501c8efe315ba67e1854b7
John A. Samuel <johnayobami77@proton.me>
2025-03-13 02:13:21 +0100

    Updates

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+
+const home = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  )
+}
+
+export default home
