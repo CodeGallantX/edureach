c87b3cf2a2a77c6039972f88007d19df176d4786
John A. Samuel <johnayobami77@proton.me>
2025-03-14 11:40:30 +0100

    Updates

--- src/components/SideIll.jsx
+++ src/components/SideIll.jsx
@@ -12,12 +12,12 @@ const SideIll = () => {
                     <h1 className="text-5xl font-bold">Experience a new Learning Journey</h1>
                     <p className="text-lg">Unlock endless opportunities with expert-led courses, interactive learning, and career-boosting skills - all in one place.</p>
                 </div>
-                <div className="bg-orange-400/50 p-6 rounded-xl space-y-3">
+                <div className="bg-[#FFC2B2] p-6 rounded-xl space-y-3 text-black">
                     <p>EduReach transformed my career! The structured courses and real-world projects gave me the confidence and skills to land my dream job. I went from a beginner to a professional in just few months!</p>
                     <div className="flex flex-row items-center justify-start gap-4">
-                        <img src="/ariana-grande.png" alt="." />
+                        <img src="/ariana-grande.png" alt="" className="w-12 h-auto rounded-full object-cover" />
                         <div className="space-y-0.5">
-                            <h4 className="text-sm">Ariana Grande</h4>
+                            <h4 className="text-sm font-bold">Ariana Grande</h4>
                             <p className="text-xs">Visual Designer, Google</p>
                         </div>
                     </div>
