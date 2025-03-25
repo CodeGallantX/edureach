be354e793168928ce3656da59679f91d634750e2
John A. Samuel <johnayobami77@proton.me>
2025-03-25 19:13:11 +0100

    Updates

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.googleapis.com https://e-sdg.onrender.com https://accounts.google.com; img-src 'self' data: https:; frame-src 'self' https://accounts.google.com; worker-src 'self' blob:;"
+      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.googleapis.com https://e-sdg.onrender.com https://accounts.google.com https://oauth2.googleapis.com; img-src 'self' data: https:; frame-src 'self' https://accounts.google.com; worker-src 'self' blob:;"
     },
   },
   optimizeDeps: {
