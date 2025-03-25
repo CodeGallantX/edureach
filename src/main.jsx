2e33b9dc3f011321a76214e71483634c829f1e9b
John A. Samuel <johnayobami77@proton.me>
2025-03-25 09:23:23 +0100

    fixed styles bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import "./index.css"
 import App from './App';
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || window.__ENV__.VITE_GOOGLE_CLIENT_ID;
