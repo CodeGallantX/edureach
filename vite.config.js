5ba6958a202e9c1a9b18258903fcf84995614187
John A. Samuel <johnayobami77@proton.me>
2025-03-25 19:57:08 +0100

    Updates

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.googleapis.com https://e-sdg.onrender.com https://accounts.google.com; img-src 'self' data: https:; frame-src 'self' https://accounts.google.com; worker-src 'self' blob:;"
-    },
-  },
-  optimizeDeps: {
-    include: ['@fontsource/manrope']
-  }
 })
