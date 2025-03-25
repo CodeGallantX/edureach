254f1d2ff6d70a8a13c5f3d134d5daed44574a21
John A. Samuel <johnayobami77@proton.me>
2025-03-25 11:54:33 +0100

    changed Google Auth call method from POST to GET

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,10 @@ export default defineConfig({
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
+      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.googleapis.com https://e-sdg.onrender.com https://accounts.google.com; img-src 'self' data: https:; frame-src 'self' https://accounts.google.com; worker-src 'self' blob:;"
     },
   },
+  optimizeDeps: {
+    include: ['@fontsource/manrope']
+  }
 })
